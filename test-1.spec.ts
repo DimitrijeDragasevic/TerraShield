6ae93aa9aa46b0d44a8737309a8c8d99ad08bd71
Dimitrije Dragasevic <dimitrije.dragasevic@gmail.com>
2023-09-05 19:17:47 +0200

    Delete test-1.spec.ts

--- test-1.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  await page.goto('https://station.terra.money/');
-  await page.getByRole('button', { name: 'Connect' }).click();
-  await page.getByRole('button', { name: 'Station (Extension)' }).click();
-});

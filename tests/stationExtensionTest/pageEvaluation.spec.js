057afd30f60d32a55e7b50d7ae261e56700d1abd
Dimitrije Dragasevic <dimitrije.dragasevic@gmail.com>
2023-09-22 15:57:27 +0200

    Fixed eval tests

--- tests/stationExtensionTest/pageEvaluation.spec.js
+++ tests/stationExtensionTest/pageEvaluation.spec.js
@@ -6,19 +6,19 @@ test.beforeEach(async ({ seedPage }) => {
   await seedPage.fillSeedForm("Test wallet 1", "Testtest123!");
   await seedPage.verifyFirstWalletAdded();
 });
-// THESE NEED TO ADAPTED
-// test('Evaluate main page', async ({homePage}) => {
-//     await homePage.evaluateMainPage()
-// });
 
-// test('Evaluate settings functionality', async ({homePage}) => {
-//     await homePage.evaluateSettings()
-// });
+test('Evaluate main page', async ({homePage}) => {
+    await homePage.evaluateMainPage()
+});
 
-// test('Evaluate manage assets functionality', async ({homePage}) => {
-//     await homePage.evaluateManageAssets()
-// });
+test('Evaluate settings functionality', async ({homePage}) => {
+    await homePage.evaluateSettings()
+});
 
-// test('Evaluate manage wallet functionality', async ({homePage}) => {
-//     await homePage.evaluateManageWallet()
-// });
+test('Evaluate manage assets functionality', async ({homePage}) => {
+    await homePage.evaluateManageAssets()
+});
+
+test('Evaluate manage wallet functionality', async ({homePage}) => {
+    await homePage.evaluateManageWallet()
+});

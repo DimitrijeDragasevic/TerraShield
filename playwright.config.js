66f1862be23c41540905c55435f2f0ecb4c8d26a
Dimitrije Dragasevic <dimitrije.dragasevic@gmail.com>
2024-02-22 13:27:44 +0100

    Add global timeout of 60 seconds

--- playwright.config.js
+++ playwright.config.js
@@ -66,7 +66,7 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  // timeout: 5 * 60 * 1000,
+  timeout: 60000,
   use: {
     trace: "on-first-retry",
   },
